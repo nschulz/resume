dd8d3d5fbcf054040f36f659631da159a034196c
Nate Schulz <ns@theomedia.us>
2014-09-27 16:37:29 -0700

    Switched to hogan.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'nateschulz.com' });
+  res.render('index', { page: {title: "nateschulz.com"}, title: 'nateschulz.com' });
 });
 
 module.exports = router;
